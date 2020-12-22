f1cb2b338475de88ddc5c1ba6876a528e808b26d
Ryze <kzh125@qq.com>
2020-12-22 15:32:47 +0800

    add product list component

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products = products;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  share() {
+    window.alert("shared");
+  }
+
+  onNotify() {
+    window.alert("onNotify");
+  }
+}
